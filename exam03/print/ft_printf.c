54e83077d143d4951a367c89cd4e6013e6c759f7
Enrico Giovanni Carbonaro <ecarbona@c2r4p2.42firenze.it>
2025-02-25 17:43:56 +0100

    ft_printf ottimizzato

--- exam03/print/ft_printf.c
+++ exam03/print/ft_printf.c
@@ -3,48 +3,60 @@
 # include <stdlib.h>
 # include <stdarg.h>
 
-size_t	ft_strlen(const char *s)
+int	ft_strlen(const char *s)
 {
-	int	i;
-
-	i = 0;
-	while (s[i] != '\0')
+	int	i = 0;
+	while(s[i])
 		i++;
 	return (i);
 }
 
-int	ft_conv(int i, unsigned long n, char *base)
+int ft_putstr(char *s)
 {
-	char	*array;
-	int		b;
+	int	i;
 
-	b = ft_strlen(base);
-	array = malloc(sizeof(char) * (i + 1));
-	if (!array)
-		return (0);
-	array[i] = '\0';
-	while (n > 0)
+	i = -1;
+	if (!s)
 	{
-		array[--i] = base[n % b];
-		n = n / b;
+		write (1, "(null)", 6);
+		return (6);
 	}
-	while (array[i])
-		write (1, &array[i++], 1);
-	free (array);
+	while (s[++i])
+		write (1, &s[i], 1);
 	return (i);
 }
 
-int	ft_putnbr_fd(int n)
+int	ft_conv(int pos, unsigned int n, char *base)
 {
-	int		temp;
-	int		i;
+	char *array;
+	int b;
 
-	i = 0;
+	b = ft_strlen(base);
+	array = malloc(pos + 1);
+	if (!array)
+		return (0);
+	array[pos] = '\0';
+	while (n > 0)
+	{
+		array[--pos] = base[n % b];
+		n = n / b;
+	}
+	while(array[pos])
+		write(1, &array[pos++], 1);
+	free(array);
+	return (pos);
+}
+
+int ft_putnbr(int n)
+{
+	int	i = 0;
+	int temp;
+
 	if (n == 0)
 		return (write(1, "0", 1), 1);
 	else if (n < 0)
 	{
-		write (1, "-", ++i);
+		write(1, "-", ++i);
 		if (n == -2147483648)
 		{
 			write(1, "2147483648", 10);
@@ -53,82 +65,55 @@ int	ft_putnbr_fd(int n)
 		n = -n;
 	}
 	temp = n;
-	while (temp > 0)
+	while(temp > 0)
 	{
 		temp /= 10;
 		i++;
 	}
-	return (ft_conv(i, n, "0123456789"));
+	return(ft_conv(i, n, "0123456789"));
 }
 
-int	ft_putchar_fd(char c, int fd)
+int ft_putbase(unsigned int n)
 {
-	write (fd, &c, 1);
-	return (1);
-}
+	int	i = 0;
+	unsigned int temp;
 
-int	ft_putstr_fd(char *s, int fd)
-{
-	int	i;
-
-	i = 0;
-	if (!s)
-	{
-		write (1, "(null)", 6);
-		return (6);
-	}
-	while (s[i] != '\0')
-	{
-		write (fd, &s[i], 1);
-		i++;
-	}
-	return (i);
-}
-
-int	ft_putbase(unsigned long n, char *base)
-{
-	unsigned long	temp;
-	int				i;
-
-	temp = n;
-	i = 0;
 	if (n == 0)
-		return (write (1, "0", 1), 1);
-	while (temp > 0)
+		return (write(1, "0", 1), 1);
+	temp = n;
+	while(temp > 0)
 	{
 		temp /= 16;
 		i++;
 	}
-	return (ft_conv(i, n, base));
+	return(ft_conv(i, n, "0123456789abcdef"));
 }
 
-static int	ft_check_arg(const char *input, va_list *arg, int *i, int *len)
+int ft_check_arg(const char *input, va_list *arg, int *i, int *len)
 {
 	if (input[*i] != '%')
 		return (0);
 	else if (input[*i + 1] == 's')
-		*len += ft_putstr_fd(va_arg(*arg, char *), 1);
+		*len += ft_putstr(va_arg(*arg, char *)); 
 	else if (input[*i + 1] == 'd')
-		*len += ft_putnbr_fd(va_arg(*arg, int));
+		*len += ft_putnbr(va_arg(*arg, int));
 	else if (input[*i + 1] == 'x')
-		*len += ft_putbase(va_arg(*arg, unsigned int), "0123456789abcdef");
-	else if (input[*i + 1] == '%')
-		*len += ft_putchar_fd('%', 1);
+		*len += ft_putbase(va_arg(*arg, unsigned int));
 	return ((*i)++, 1);
 }
 
 int	ft_printf(const char *input, ...)
 {
-	va_list	arg;
-	int		i;
-	int		len;
+	va_list arg;
+	int     i;
+	int     len;
 
 	i = -1;
 	len = 0;
 	va_start(arg, input);
 	while (input[++i])
 	{
-		if (ft_check_arg(input, &arg, &i, &len) == 0)
+		if (!ft_check_arg(input, &arg, &i, &len))
 		{
 			write(1, &input[i], 1);
 			len++;
@@ -138,9 +123,3 @@ int	ft_printf(const char *input, ...)
 	return (len);
 }
 
-// int main()
-// {
-// 	printf("%s: %d, %x\n", "prova", 10, 88);
-// 	ft_printf("%s: %d, %x\n", "prova", 10, 88);
-// 	return (0);
-// }
